750728a4b5ca0b83cb903b13e69a35a2631be804
blqck6ird <ad.abhijeetadarsh@gmail.com>
2023-01-31 17:39:49 +0530

    done

--- 24jan.sql
+++ 24jan.sql
@@ -1,4 +1,5 @@
-/* create table branch(
+/*
+create table branch(
 	branch_name varchar(20),
 	branch_city varchar(20),
 	assets int NOT NULL,
@@ -8,22 +9,29 @@ create table customer(
 	customer_name varchar(20),
 	branch_name varchar(20),
 	customer_city varchar(20),
+	PRIMARY KEY (customer_name),
 	FOREIGN KEY(branch_name) REFERENCES branch(branch_name)
 );
 create table loan(
 	loan_number varchar(20),
 	branch_name varchar(20),
 	amount int,
+	PRIMARY KEY (loan_number),
 	FOREIGN KEY(branch_name) REFERENCES customer(branch_name)
 );
-*/
+
+ALTER TABLE customer
+ADD PRIMARY KEY (customer_name);
+
+ALTER TABLE loan
+ADD PRIMARY KEY (loan_number);
+
 create table borrower(
 	customer_name varchar(20),
 	loan_number varchar(20),
 	FOREIGN KEY(customer_name) REFERENCES customer(customer_name),
 	FOREIGN KEY(loan_number) REFERENCES loan(loan_number)
 );
-/*
 create table account(
 	account_number int,
 	branch_name varchar(20),
@@ -31,39 +39,76 @@ create table account(
 	FOREIGN KEY(branch_name) REFERENCES branch(branch_name)
 );
 
-insert into table branch values('dhanbad001','dhanbad',100000);
-insert into table branch values('dhanbad002','dhanbad',70000);
-insert into table branch values('patna001','patna',200000);
-insert into table branch values('kolkata023','kolkata',150000);
-insert into table branch values('rishikesh045','rishikesh',75000);
+insert into branch values('SBI001D','Dhanbad',100000);
+insert into branch values('PNB002T','Tatanagar',70000);
+insert into branch values('YBI002P','Patna',200000);
+insert into branch values('ISI002K','Kolkata',150000);
+insert into branch values('BOB011R','Rishikesh',75000);
 
-insert into table customer values('abhijeet','dhanbad001',20000);
-insert into table customer values('amit','dhanbad002',40000);
-insert into table customer values('rohan','patna001',10000);
-insert into table customer values('ankit);
-insert into table customer values();
+insert into customer values('Liam','SBI001D','Patna');
+insert into customer values('John','PNB002T','Bombay');
+insert into customer values('Luke','YBI002P','Delhi');
+insert into customer values('Levi','BOB011R','Shimla');
+insert into customer values('Emma','SBI001D','Gaya');
 
-insert into table loan values();
-insert into table loan values();
-insert into table loan values();
-insert into table loan values();
-insert into table loan values();
+insert into loan values('5001','SBI001D',50001);
+insert into loan values('5002','PNB002T',5000);
+insert into loan values('5003','YBI002P',900);
+insert into loan values('5004','BOB011R',10000);
+insert into loan values('5005','SBI001D',75000);
 
-insert into table borrower values();
-insert into table borrower values();
-insert into table borrower values();
-insert into table borrower values();
-insert into table borrower values();
+UPDATE loan
+    -> SET amount=50001
+    -> WHERE loan_number='5001';
 
-insert into table account values();
-insert into table account values();
-insert into table account values();
-insert into table account values();
-insert into table account values();
+UPDATE loan SET amount=75000 WHERE loan_number='5005';
 
 
-CREATE VIEW Query1(C_Name,B_Name,B_city)
+insert into borrower values('Emma','5001');
+insert into borrower values('Liam','5005');
+insert into borrower values('John','5002');
+insert into borrower values('Luke','5003');
+insert into borrower values('Levi','5004');
+
+insert into account values(15025057,'PNB002T',150000);
+insert into account values(15025058,'ISI002K',50000);
+insert into account values(15025059,'SBI001D',20000);
+insert into account values(15025060,'YBI002P',1000);
+insert into account values(15025061,'BOB011R',200000);
+
+*/
+/*
+
+-- 1.
+CREATE or REPLACE VIEW Query1(C_Name,B_Name,B_city)
 AS SELECT customer.customer_name, branch.branch_name, branch.branch_city
-FROM customer, branch, loan
-WHERE loan,amount>50000 and loan.branch_name and customer.branch_name=branch.branch_name;
+FROM customer, branch, loan, borrower
+WHERE loan.amount>50000 and loan.loan_number=borrower.loan_number and customer.customer_name=borrower.customer_name and loan.branch_name=branch.branch_name;
+
+-- 2.
+CREATE or REPLACE VIEW Query2(C_Name)
+AS SELECT customer.customer_name
+FROM customer
+WHERE customer.customer_city='Bombay';
+
+-- 3.
+CREATE or REPLACE VIEW Query3(ACC_Number,B_Name,B_Assets)
+AS SELECT account.account_number, branch.branch_name, branch.assets
+FROM account, branch
+WHERE account.balance>100000 and branch.branch_name=account.branch_name;
+
+-- 4.
+CREATE or REPLACE VIEW Query4(C_Name,B_Name,B_city,Lona_no)
+AS SELECT customer.customer_name, customer.branch_name, branch.branch_city, borrower.loan_number
+FROM customer, branch, borrower
+WHERE customer.customer_name=borrower.customer_name and customer.branch_name=branch.branch_name;
+
+-- 5.
+DELETE borrower FROM borrower INNER JOIN loan WHERE loan.amount<1000 and borrower.loan_number=loan.loan_number;
+
+-- 6.
+UPDATE loan, branch
+SET loan.amount = loan.amount + 10000
+WHERE branch.assets>80000 and branch.branch_name=loan.branch_name;
+
 */
--- tmp/31jan.sql
+++ tmp/31jan.sql
@@ -3,8 +3,9 @@ create table Owner(
 	driver_id varchar(20),
 	name varchar(20),
 	hometown varchar(40),
-	birthyear int,
-	PRIMARY KEY(driver_id)
+	birthyear int NOT NULL,
+	PRIMARY KEY(driver_id),
+	CHECK (2023-birthyear>=18)
 );
 create table accident_record(
 	report_number varchar(20),
@@ -25,34 +26,80 @@ create table compensation_record(
 insert into Owner values('BR001','Liam','patna',2000);
 insert into Owner values('UP001','Noah','kanpur',1995);
 insert into Owner values('JH001','John','dhanbad',2000);
-insert into Owner values('UP002','Owen','banaras',2005);
+insert into Owner values('UP002','Owen','banaras',1985);
 insert into Owner values('RJ001','Luke','kota',2001);
 insert into Owner values('MP001','Jack','ujjain',2003);
 insert into Owner values('CG001','Levi','raipur',2004);
 
 
 insert into accident_record values('R141',2010,'patna','BR001');
-insert into accident_record values('R145',2021,'gaya','JH001');
-insert into accident_record values('R149',2022,'delhi','UP001');
-insert into accident_record values('R160',2023,'dhanbad','UP001');
-insert into accident_record values('R169',2015,'raipur','MP001');
+insert into accident_record values('R146',2011,'kanpur','UP001');
+insert into accident_record values('R151',2015,'dhanbad','JH001');
+insert into accident_record values('R156',2017,'kota','UP001');
+insert into accident_record values('R161',2018,'raipur','UP002');
+insert into accident_record values('R162',2018,'gaya','RJ001');
+insert into accident_record values('R163',2020,'lucknow','MP001');
+insert into accident_record values('R164',2021,'kota','CG001');
+insert into accident_record values('R169',2022,'delhi','CG001');
 
 
 
 insert into compensation_record values('R141',20000);
-insert into compensation_record values('R145',200000);
-insert into compensation_record values('R149',300000);
-insert into compensation_record values('R160',50000);
-insert into compensation_record values('R169',75000);
+insert into compensation_record values('R146',200000);
+insert into compensation_record values('R151',300000);
+insert into compensation_record values('R156',50000);
+insert into compensation_record values('R161',75000);
+insert into compensation_record values('R162',5000);
+insert into compensation_record values('R163',10000);
+insert into compensation_record values('R164',100000);
+insert into compensation_record values('R169',90000);
 
-
+-- /*
 -- 1.
 ALTER TABLE Owner
 ADD car_model varchar(30)
 AFTER name;
 
-*/
-
+UPDATE Owner SET car_model = 'Nexon' WHERE driver_id = 'BR001';
+UPDATE Owner SET car_model = 'Kia. Sonet' WHERE driver_id = 'UP001';
+UPDATE Owner SET car_model = 'Tata. Punch' WHERE driver_id = 'JH001';
+UPDATE Owner SET car_model = 'Renault. Kiger' WHERE driver_id = 'UP002';
+UPDATE Owner SET car_model = 'Nissan. Magnite' WHERE driver_id = 'RJ001';
+UPDATE Owner SET car_model = 'Hyundai' WHERE driver_id = 'MP001';
+UPDATE Owner SET car_model = 'Mercedes-Benz' WHERE driver_id = 'CG001';
 
 -- 2.
+SELECT COUNT(report_number)
+FROM accident_record
+WHERE accident_year=2018;
 
+
+-- 3.
+
+SELECT SUM(damage_amount),AVG(damage_amount)
+FROM compensation_record;
+
+-- 4.
+SELECT location, accident_year FROM accident_record WHERE report_number='R141';
+
+-- 5.
+CREATE or REPLACE VIEW Query1(Driver_Id,Name,Car_Model,Report_Number,Damage_Amount)
+AS SELECT Owner.driver_id,Owner.name,Owner.car_model,accident_record.report_number,compensation_record.damage_amount
+FROM Owner,accident_record,compensation_record
+WHERE accident_record.accident_year > 2020 AND Owner.driver_id = accident_record.driver_id AND accident_record.report_number = compensation_record.report_number;
+
+-- 6.
+CREATE or REPLACE VIEW Query2(Driver_Id,Driver_Name,Report_Number)
+AS SELECT Owner.driver_id,Owner.name,compensation_record.report_number
+FROM Owner,accident_record,compensation_record
+WHERE compensation_record.damage_amount < 210000 AND Owner.driver_id = accident_record.driver_id AND accident_record.report_number = compensation_record.report_number;
+
+
+-- 7.
+UPDATE compensation_record SET damage_amount = 300000 WHERE report_number = 'R169';
+
+-- 8.
+SELECT Max(damage_amount), Min(damage_amount)
+FROM compensation_record;
+
+*/
