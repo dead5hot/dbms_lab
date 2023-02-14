8e4a55bb1d81e79d5f74ac529ca4c647416f633c
Your Name <you@example.com>
2023-02-14 12:55:26 +0530

    lab6work

--- lab6/14feb.sql
+++ lab6/14feb.sql
@@ -1,24 +1,28 @@
 -- Sailor (Sid, Sname, Rating, Age),
 -- Reserve (Sid, Bid, Date),
 -- Boat (Bid, Bname, Color)
-
+/*
 CREATE TABLE Sailor(
-    Sid int,
-    Sname varchar(30),
-    Rating int,
-    Age real
-);
-
-CREATE TABLE Reserve(
-    Sid int,
-    Bid int,
-    Date varchar(12)
+	Sid int,
+	Sname varchar(30),
+	Rating int,
+	Age real,
+	PRIMARY KEY(Sid)
 );
 
 CREATE TABLE Boat(
-    Bid int,
-    Bname varchar(30),
-    Color varchar(20)
+	Bid int,
+	Bname varchar(30),
+	Color varchar(20),
+	PRIMARY KEY(Bid)
+);
+
+CREATE TABLE Reserve(
+	Sid int,
+	Bid int,
+	Date date,
+	FOREIGN KEY(Sid) REFERENCES Sailor(Sid),
+	FOREIGN KEY(Bid) REFERENCES Boat(Bid)
 );
 
 INSERT INTO Sailor VALUES( 22, "Dustin" ,  7  , 45   );
@@ -32,6 +36,10 @@ INSERT INTO Sailor VALUES( 71, "Zorba"  ,  10 , 16   );
 INSERT INTO Sailor VALUES( 74, "Horatio",  9  , 35   );
 INSERT INTO Sailor VALUES( 95, "Bob"    ,  3  , 63.5 );
 
+INSERT INTO Boat VALUES( 101, "Interlake", "Blue");
+INSERT INTO Boat VALUES( 102, "Interlake", "Red");
+INSERT INTO Boat VALUES( 103, "Clipper", "Green");
+INSERT INTO Boat VALUES( 104, "Marine", "Red");
 
 INSERT INTO Reserve VALUES( 22, 101, "1998-10-10");
 INSERT INTO Reserve VALUES( 22, 102, "1998-10-10");
@@ -45,12 +53,65 @@ INSERT INTO Reserve VALUES( 64, 102, "1998-08-09");
 INSERT INTO Reserve VALUES( 74, 103, "1998-08-09");
 INSERT INTO Reserve VALUES( 95, 101, "1998-10-10");
 
-
-INSERT INTO Boat VALUES( 101, "Interlake", "Blue");
-INSERT INTO Boat VALUES( 102, "Interlake", "Red");
-INSERT INTO Boat VALUES( 103, "Clipper", "Green");
-INSERT INTO Boat VALUES( 104, "Marine", "Red");
-
 SELECT * FROM Sailor;
 SELECT * FROM Reserve;
 SELECT * FROM Boat;
+
+*/
+
+
+
+-- 1.
+SELECT S.Sid, S.Sname
+FROM Sailor S
+WHERE S.Rating > ANY (
+	SELECT S2.Rating
+	FROM Sailor S2
+	WHERE S2.Sname = 'Art'
+);
+
+-- 2.
+SELECT S.Sid, S.Sname
+FROM Sailor S
+WHERE S.Rating >= ALL (
+	SELECT S2.Rating
+	FROM Sailor S2
+);
+
+-- 3.
+SELECT S.Sid, S.Sname
+FROM Sailor S
+WHERE S.Rating <= ALL (
+	SELECT S2.Rating
+	FROM Sailor S2
+);
+
+-- 4.
+SELECT *
+FROM Boat B
+WHERE B.Color NOT IN (
+	SELECT B2.Color
+	FROM Boat B2
+	WHERE B2.Color = 'Red'
+);
+
+-- 5.
+SELECT *
+FROM Boat B
+WHERE B.Bname != 'Interlake';
+
+-- 6.
+SELECT *
+FROM Sailor S
+WHERE S.Sname IN (
+	'Zorba',
+	'Horatio',
+	'Dustin'
+);
+
+-- 7.
+SELECT *
+FROM Boat B
+WHERE Color = ANY (
+	Color = 'Red' OR Color = 'Blue' OR Color = 'Black'
+);
